91a818b47624091ab42dc28b474fe7bd6577657e
xOMG0857x <77949530+xOMG0857x@users.noreply.github.com>
2021-01-25 15:37:53 +0800

    Add files via upload

---
+++ D1-2.py
@@ -0,0 +1,14 @@
+marks=int(input('分數?'))
+if 0<=marks<=100
+    if 90<=marks<=100:
+    print('A')
+    elif 80<=marks<90:
+    print('B')
+    elif 70<=marks<80:
+    print('C')
+    elif 60<=marks<70:
+    print('D')
+    else:
+    print('E')
+else:
+    print('輸入0到100')    
